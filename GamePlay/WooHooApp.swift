3b596a51e6fa2b6c72da22bd3fa974fd801db5f3
fhassidiqi <94043950+falahhasbiassidiqi@users.noreply.github.com>
2023-03-24 13:25:47 +0700

    Add files via upload

---
+++ GamePlay/WooHooApp.swift
@@ -0,0 +1,17 @@
+//
+//  WooHooApp.swift
+//  WooHoo
+//
+//  Created by Falah Hasbi Assidiqi on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WooHooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
